feb7c206877a4442c989b6b043f8201e2e505bb7
levi81770 <levi77081@gmail.com>
2026-01-14 00:09:51 -0500

    wrote some incomplit functions and event listiners, need to be modified.

--- js/app.js
+++ js/app.js
@@ -6,7 +6,7 @@ const discardPile = []
 const humanPlayer = []
 const computerPlayer = []
 const colors = ['red', 'green', 'yellow', 'blue']
-
+const actionCards = ['skip', 'draw-two', 'revers']
 // const randomCard = Math.floor(Math.random() * deck.length)
 
 /*---------------------------- Variables (state) ----------------------------*/
@@ -15,9 +15,12 @@ let turn
 
 
 /*------------------------ Cached Element References ------------------------*/
+const discardPileDiv = document.querySelector('.discard-pile')
+const deckDiv = document.querySelector('.deck')
 const humanHand = document.querySelector('.human-player')
 const computerHand = document.querySelector('.computer-player')
-
+const msgEl = document.querySelector('#msg')
+const drawCard = document.querySelector('.draw-card')
 /*-------------------------------- Functions --------------------------------*/
 function fillDeckWithAllCards() {
     colors.forEach(color => {
@@ -57,12 +60,12 @@ function fillDeckWithAllCards() {
 
         const drawTwo1 = document.createElement('div')
         drawTwo1.classList.add(`${color}`, 'draw-two', 'card')
-        drawTwo1.textContent = 'draw-two'
+        drawTwo1.textContent = '+2'
         deck.push(drawTwo1)
 
         const drawTwo2 = document.createElement('div')
         drawTwo2.classList.add(`${color}`, 'draw-two', 'card')
-        drawTwo2.textContent = 'draw-two'
+        drawTwo2.textContent = '+2'
         deck.push(drawTwo2)
 
         const wild = document.createElement('div')
@@ -99,12 +102,130 @@ function dealCardsToPlayers() {
         computerHand.appendChild(computerPlayer[i])
     }
 }
+
+const updateDiscardPileDisplay = () => {
+    discardPileDiv.innerHTML = ''
+    discardPileDiv.appendChild(discardPile.at(-1))
+}
+
+function startDiscardPile() {
+    discardPile.push(deck.shift())
+
+    while (discardPile.at(-1).classList.contains('wild-draw-four')) {
+        deck.push(discardPile.shift())
+        shuffleDeck(deck)
+        discardPile.push(deck.shift())
+    }
+
+    updateDiscardPileDisplay()
+}
+
+function isValidCard(cardToPlay, currentDiscard) {
+    if (cardToPlay.classList.contains('wild') || cardToPlay.classList.contains('wild-draw-four')) {
+        return true
+    }
+
+    if (currentDiscard.classList.contains('wild') || currentDiscard.classList.contains('wild-draw-four')) {
+        return true
+    }
+
+    for (let i = 0; i < 10; i++) {
+        if (cardToPlay.classList.contains(`${i}`) && currentDiscard.classList.contains(`${i}`)) {
+            return true
+        }
+    }
+
+    for (let i = 0; i < colors.length - 1; i++) {
+        if (cardToPlay.classList.contains(`${colors[1]}`) && currentDiscard.classList.contains(`${colors[1]}`)) {
+            return true
+        }
+    }
+
+    for (let i = 0; i < actionCards.length - 1; i++) {
+        if (cardToPlay.classList.contains(`${actionCards[1]}`) && currentDiscard.classList.contains(`${actionCards[1]}`)) {
+            return true
+        }
+    }
+
+    return false
+}
+
+function computerDrawCard() {
+    computerPlayer.push(deck.shift())
+    computerHand.appendChild(computerPlayer.at(-1))
+}
+
+function drawCardHandler() {
+    humanPlayer.push(deck.shift())
+    humanHand.appendChild(humanPlayer.at(-1))
+}
+
+function handelHumanCardClick(event) {
+    const clickedCard = event.target
+    let clickedCardIndex = humanPlayer.indexOf(clickedCard)
+
+    if (!clickedCard.classList.contains('card')) return
+    if (turn === 'player 2') return
+
+    if (isValidCard(clickedCard, discardPile.at(-1))) {
+        humanPlayer.splice(clickedCardIndex, 1)
+        clickedCard.remove()
+        discardPile.push(clickedCard)
+        updateDiscardPileDisplay()
+        switchPlayerTurn()
+    } else {
+        msgEl.textContent = 'not a valid card'
+    }
+}
+
+function computerTurn() {
+    const randomComCard = Math.floor(Math.random() * computerPlayer.length)
+    let validComCards = []
+    
+    for (let i = 0; i < computerPlayer.length - 1; i++) {
+        if (isValidCard(computerPlayer[i], discardPile.at(-1))) {
+            validComCards.push(computerPlayer[i])
+        }
+    }
+
+    discardPile.push(validComCards[randomComCard])
+    updateDiscardPileDisplay()
+
+    if (validComCards.length === 0) {
+        computerDrawCard()
+    }
+
+    switchPlayerTurn()
+}
+
+function switchPlayerTurn() {
+    if (winner) return;
+    turn = turn === 'player 1' ? 'player 2' : 'player 1'
+}
+// function playCard(playerHand, currentDiscard, playerArr) {
+//     let removedCard
+//     playerHand.forEach((card, cardIndex) => {
+//         if (isValidCard(card, currentDiscard)) {
+//             card.addEventListener('click', function() {
+//                 removedCard = playerArr.splice(cardIndex, 1)
+//                 discardPile.push(removedCard.at(-1))
+//                 playerHand.splice(cardIndex, 1)
+//             })
+//         } else if (!isValidCard(card, currentDiscard)) {
+
+//             msgEl.textContent = 'not a valid card'
+//         }
+//     })
+// }
 fillDeckWithAllCards()
 shuffleDeck(deck)
 dealCardsToPlayers()
-console.log(humanPlayer, computerPlayer);
+startDiscardPile()
+console.log(humanPlayer, computerPlayer, deck, discardPile);
 
 /*----------------------------- Event Listeners -----------------------------*/
 
+humanHand.addEventListener('click', handelHumanCardClick)
 
+drawCard.addEventListener('click', drawCardHandler)
 
